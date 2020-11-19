0ebd07c785612a79393e4159e0920a56f344f6e5
nibble2 <nibblekk@gmail.com>
2020-11-20 02:16:04 +0900

    Create #5 - Separate routers file

--- app.js
+++ app.js
@@ -3,20 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import {
-  userRouter
-} from "./router";
-
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const app = express();
-
-const handleHome = (req, res) => {
-  res.send("Hello nodemon");
-};
-
-const handleProfile = (req, res) => {
-  res.send("You are on my profile");
-};
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -26,11 +17,8 @@ app.use(bodyParser.urlencoded({
 app.use(helmet());
 app.use(morgan("dev"));
 
-
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
---
+++ routers/globalRouter.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+const globalRouter = express.Router();
+
+export default globalRouter;
