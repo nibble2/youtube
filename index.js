601ac719abe5f612ba2c2413fd26ad2d97a11789
nibble2 <nibblekk@gmail.com>
2020-11-19 23:27:21 +0900

    Update #1 - Using req, res method

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+    res.send("Hello Home");
+}
+
+function handleProfile(req, res) {
+    res.send("Your on my profile");
+}
+
+app.get("/profile", handleProfile);
+app.get("/", handleHome);
 app.listen(PORT, handleListening);
