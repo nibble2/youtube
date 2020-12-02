e902e65544097d9f1fda545bd6e0a77940a344d0
nibble2 <nibblekk@gmail.com>
2020-12-03 03:18:25 +0900

    Update - search controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -19,16 +19,25 @@ export const home = async (req, res) => {
 	}
 };
 
-export const search = (req, res) => {
-	// console.log(videos);
+export const search = async (req, res) => {
 	const {
 		query: { term: searchingBy },
 	} = req;
+	let videos = [];
+	try {
+		videos = await Video.find({
+			title: { $regex: searchingBy, $options: 'i' },
+		});
+	} catch (error) {
+		console.log(error);
+	}
 	res.render('search', {
 		pageTitle: 'Search',
 		searchingBy,
+		videos,
 	});
 };
+
 export const getUpload = (req, res) =>
 	res.render('upload', {
 		pageTitle: 'Upload',
