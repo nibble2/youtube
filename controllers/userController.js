974cb7aba7b71b08eefc7da3679de54ee01aaf7c
nibble2 <nibblekk@gmail.com>
2020-12-03 00:08:11 +0900

    Update - videoDetail.pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,50 +1,48 @@
-import routes from "../routes";
+import routes from '../routes';
 export const getJoin = (req, res) => {
-    res.render("join", {
-        pageTitle: 'Join'
-    });
+	res.render('join', {
+		pageTitle: 'Join',
+	});
 };
 export const postJoin = (req, res) => {
-    const {
-        body: {
-            name,
-            email,
-            password,
-            passwordCheck
-        }
-    } = req;
-    if (password !== passwordCheck) {
-        res.status(400);
-        res.render("join", {
-            pageTitle: 'Join'
-        });
-    } else {
-        //TODO - Register User
-        //TODO - Log user in
-        res.redirect(routes.home)
-    }
-
-}
+	const {
+		body: { name, email, password, passwordCheck },
+	} = req;
+	if (password !== passwordCheck) {
+		res.status(400);
+		res.render('join', {
+			pageTitle: 'Join',
+		});
+	} else {
+		//TODO - Register User
+		//TODO - Log user in
+		res.redirect(routes.home);
+	}
+};
 
 export const getLogin = (req, res) => {
-    res.render("login", {
-        pageTitle: 'Login'
-    });
-}
+	res.render('login', {
+		pageTitle: 'Login',
+	});
+};
 
 export const postLogin = (req, res) => {
-    res.redirect(routes.home);
-}
+	res.redirect(routes.home);
+};
 
-export const logout = (req, res) => res.render("logout", {
-    pageTitle: 'Logout'
-});
-export const userDetail = (req, res) => res.render("userDetail", {
-    pageTitle: 'User Detail'
-});
-export const editProfile = (req, res) => res.render("editProfile", {
-    pageTitle: 'Edit Profile'
-});
-export const changePassword = (req, res) => res.render("changePassword", {
-    pageTitle: 'Change Password'
-});
+export const logout = (req, res) =>
+	res.render('logout', {
+		pageTitle: 'Logout',
+	});
+export const userDetail = (req, res) =>
+	res.render('userDetail', {
+		pageTitle: 'User Detail',
+	});
+export const editProfile = (req, res) =>
+	res.render('editProfile', {
+		pageTitle: 'Edit Profile',
+	});
+export const changePassword = (req, res) =>
+	res.render('changePassword', {
+		pageTitle: 'Change Password',
+	});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,17 +1,18 @@
-import routes from "../routes";
-import Video from "../models/Video";
+import routes from '../routes';
+import Video from '../models/Video';
 
 export const home = async (req, res) => {
   try {
     const videos = await Video.find({}); // DB에 있는 모든 Video 가져옴
-    res.render("home", {
-      pageTitle: "Home",
+    console.log(videos);
+    res.render('home', {
+      pageTitle: 'Home',
       videos,
     });
   } catch (error) {
     console.log(error);
-    res.render("home", {
-      pageTitle: "Home",
+    res.render('home', {
+      pageTitle: 'Home',
       videos: [],
     });
   }
@@ -24,15 +25,15 @@ export const search = (req, res) => {
       term: searchingBy
     },
   } = req;
-  res.render("search", {
-    pageTitle: "Search",
+  res.render('search', {
+    pageTitle: 'Search',
     searchingBy,
     videos,
   });
 };
 export const getUpload = (req, res) =>
-  res.render("upload", {
-    pageTitle: "Upload",
+  res.render('upload', {
+    pageTitle: 'Upload',
   });
 
 export const postUpload = async (req, res) => {
@@ -43,25 +44,39 @@ export const postUpload = async (req, res) => {
     },
     file: {
       path
-    }
+    },
   } = req;
   const newVideo = await Video.create({
     fileUrl: path,
     title,
-    description
+    description,
   });
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", {
-    pageTitle: "Video Detail",
-  });
+export const videoDetail = async (req, res) => {
+  const {
+    params: {
+      id
+    }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    console.log(video);
+    res.render("videoDetail", {
+      pageTitle: "video Detail",
+      video
+    });
+  } catch (error) {
+    console.log("error");
+  }
+};
+
 export const editVideo = (req, res) =>
-  res.render("editVideo", {
-    pageTitle: "Edit Video",
+  res.render('editVideo', {
+    pageTitle: 'Edit Video',
   });
 export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", {
-    pageTitle: "Delete Video",
+  res.render('deleteVideo', {
+    pageTitle: 'Delete Video',
   });
