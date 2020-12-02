705b000ba9598175d1fe4114607e9ffb029f3319
nibble2 <nibblekk@gmail.com>
2020-12-02 19:42:14 +0900

    Update - Import video model

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -2,56 +2,60 @@ import routes from "../routes";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-    try {
-        res.render("home", {
-            pageTitle: 'Home',
-            videos
-        });
-    } catch (error) {
-        console.log(error);
-        res.render("home", {
-            pageTitle: 'Home',
-            videos: []
-        });
-    }
-}
+  try {
+    const videos = await Video.find({}); // DB에 있는 모든 Video 가져옴
+    res.render("home", {
+      pageTitle: "Home",
+      videos,
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("home", {
+      pageTitle: "Home",
+      videos: [],
+    });
+  }
+};
 
 export const search = (req, res) => {
-    console.log(videos);
-    const {
-        query: {
-            term: searchingBy
-        }
-    } = req;
-    res.render("search", {
-        pageTitle: 'Search',
-        searchingBy,
-        videos
-    });
+  console.log(videos);
+  const {
+    query: {
+      term: searchingBy
+    },
+  } = req;
+  res.render("search", {
+    pageTitle: "Search",
+    searchingBy,
+    videos,
+  });
 };
-export const getUpload = (req, res) => res.render("upload", {
-    pageTitle: 'Upload'
-});
+export const getUpload = (req, res) =>
+  res.render("upload", {
+    pageTitle: "Upload",
+  });
 
 export const postUpload = (req, res) => {
-    const {
-        body: {
-            file,
-            title,
-            description
-        }
-    } = req;
-    res.redirect(routes.videoDetail(7777));
-    //TODO - Upload and save video
+  const {
+    body: {
+      file,
+      title,
+      description
+    },
+  } = req;
+  res.redirect(routes.videoDetail(7777));
+  //TODO - Upload and save video
+};
 
-}
-
-export const videoDetail = (req, res) => res.render("videoDetail", {
-    pageTitle: 'Video Detail'
-});
-export const editVideo = (req, res) => res.render("editVideo", {
-    pageTitle: 'Edit Video'
-});
-export const deleteVideo = (req, res) => res.render("deleteVideo", {
-    pageTitle: 'Delete Video'
-});
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", {
+    pageTitle: "Video Detail",
+  });
+export const editVideo = (req, res) =>
+  res.render("editVideo", {
+    pageTitle: "Edit Video",
+  });
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", {
+    pageTitle: "Delete Video",
+  });
