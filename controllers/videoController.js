884c3f3ee74105c21c629d5a36daef580a061766
nibble2 <nibblekk@gmail.com>
2020-12-03 02:44:51 +0900

    Update - Install eslint

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -2,129 +2,114 @@ import routes from '../routes';
 import Video from '../models/Video';
 
 export const home = async (req, res) => {
-  try {
-    const videos = await Video.find({}).sort({
-      _id: -1
-    }); // DB에 있는 모든 Video 가져옴
-    res.render('home', {
-      pageTitle: 'Home',
-      videos,
-    });
-  } catch (error) {
-    // console.log(error);
-    res.render('home', {
-      pageTitle: 'Home',
-      videos: [],
-    });
-  }
+	try {
+		const videos = await Video.find({}).sort({
+			_id: -1,
+		}); // DB에 있는 모든 Video 가져옴
+		res.render('home', {
+			pageTitle: 'Home',
+			videos,
+		});
+	} catch (error) {
+		// console.log(error);
+		res.render('home', {
+			pageTitle: 'Home',
+			videos: [],
+		});
+	}
 };
 
 export const search = (req, res) => {
-  // console.log(videos);
-  const {
-    query: {
-      term: searchingBy
-    },
-  } = req;
-  res.render('search', {
-    pageTitle: 'Search',
-    searchingBy,
-    videos,
-  });
+	// console.log(videos);
+	const {
+		query: { term: searchingBy },
+	} = req;
+	res.render('search', {
+		pageTitle: 'Search',
+		searchingBy,
+	});
 };
 export const getUpload = (req, res) =>
-  res.render('upload', {
-    pageTitle: 'Upload',
-  });
+	res.render('upload', {
+		pageTitle: 'Upload',
+	});
 
 export const postUpload = async (req, res) => {
-  const {
-    body: {
-      title,
-      description
-    },
-    file: {
-      path
-    },
-  } = req;
-  const newVideo = await Video.create({
-    fileUrl: path,
-    title,
-    description,
-  });
-  res.redirect(routes.videoDetail(newVideo.id));
+	const {
+		body: { title, description },
+		file: { path },
+	} = req;
+	const newVideo = await Video.create({
+		fileUrl: path,
+		title,
+		description,
+	});
+	res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = async (req, res) => {
-  const {
-    params: {
-      id
-    }
-  } = req;
-  try {
-    const video = await Video.findById(id);
-    res.render("videoDetail", {
-      pageTitle: video.title,
-      video
-    });
-  } catch (error) {
-    res.redirect(routes.home);
-  }
+	const {
+		params: { id },
+	} = req;
+	try {
+		const video = await Video.findById(id);
+		res.render('videoDetail', {
+			pageTitle: video.title,
+			video,
+		});
+	} catch (error) {
+		res.redirect(routes.home);
+	}
 };
 
 export const getEditVideo = async (req, res) => {
-  const {
-    params: {
-      id
-    }
-  } = req;
-  try {
-    const video = await Video.findById(id);
-    res.render("editVideo", {
-      pageTitle: "editVideo",
-      video
-    });
-  } catch (error) {
-    res.redirect(routes.home);
-  }
-}
+	const {
+		params: { id },
+	} = req;
+	try {
+		const video = await Video.findById(id);
+		res.render('editVideo', {
+			pageTitle: 'editVideo',
+			video,
+		});
+	} catch (error) {
+		res.redirect(routes.home);
+	}
+};
 
 export const postEditVideo = async (req, res) => {
-  console.log("hello");
-  const {
-    params: {
-      id
-    },
-    body: {
-      title,
-      description
-    }
-  } = req;
+	console.log('hello');
+	const {
+		params: { id },
+		body: { title, description },
+	} = req;
 
-  try {
-    await Video.findByIdAndUpdate({
-      _id: id
-    }, {
-      title,
-      description
-    });
-    res.redirect(routes.videoDetail(video.id));
-  } catch (error) {
-    res.redirect(routes.home)
-  }
-}
-
+	try {
+		await Video.findByIdAndUpdate(
+			{
+				_id: id,
+			},
+			{
+				title,
+				description,
+			},
+		);
+		res.redirect(routes.videoDetail(video.id));
+	} catch (error) {
+		res.redirect(routes.home);
+	}
+};
 
 export const deleteVideo = async (req, res) => {
-  const {
-    params: {
-      id
-    }
-  } = req;
-  try {
-    await Video.findByIdAndRemove({
-      _id: id
-    });
-  } catch (error) {}
-  res.redirect(routes.home);
-}
+	const {
+		params: { id },
+	} = req;
+	try {
+		await Video.findByIdAndRemove({
+			_id: id,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+	res.redirect(routes.home);
+};
