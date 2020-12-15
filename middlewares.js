7db62f41f20baccee73255446ed68c9d0cacb286
nibble2 <nibblekk@gmail.com>
2020-12-16 04:20:41 +0900

    Update - Prevent collision of routes

--- middlewares.js
+++ middlewares.js
@@ -13,4 +13,21 @@ export const localsMiddleware = (req, res, next) => {
 	next();
 };
 
+//로그인 안한상태
+export const onlyPublic = (req, res, next) => {
+	if (req.user) {
+		res.redirect(routes.home);
+	} else {
+		next();
+	}
+}
+
+export const onlyPrivate = (req, res, next) => {
+	if (req.user) {
+		next();
+	} else {
+		res.redirect(routes.home);
+	}
+}
+
 export const uploadVideo = multerVideo.single('videoFile');
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -9,22 +9,23 @@ import {
     deleteVideo
 } from "../controllers/videoController";
 import {
-    uploadVideo
+    uploadVideo,
+    onlyPrivate
 } from "../middlewares";
 
 const videoRouter = express.Router();
 
 // video Upload
-videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, uploadVideo, postUpload);
+videoRouter.get(routes.upload, onlyPrivate, getUpload);
+videoRouter.post(routes.upload, onlyPrivate, uploadVideo, postUpload);
 
 // video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
 
 //video Edit
-videoRouter.get(routes.editVideo(), getEditVideo);
-videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.editVideo(), onlyPrivate, getEditVideo);
+videoRouter.post(routes.editVideo(), onlyPrivate, postEditVideo);
 
-videoRouter.get(routes.deleteVideo(), deleteVideo);
+videoRouter.get(routes.deleteVideo(), onlyPrivate, deleteVideo);
 
 export default videoRouter;
