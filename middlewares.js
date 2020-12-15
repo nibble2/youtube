c654e5af9bce2bc286a035e16fd1bed005d6d5b7
nibble2 <nibblekk@gmail.com>
2020-12-16 00:28:01 +0900

    Update - Install npm express-session

--- middlewares.js
+++ middlewares.js
@@ -1,15 +1,16 @@
-import multer from "multer";
-import routes from "./routes";
+import multer from 'multer';
+import routes from './routes';
 
 const multerVideo = multer({
-    dest: "uploads/videos/"
+	dest: 'uploads/videos/',
 });
 
 export const localsMiddleware = (req, res, next) => {
-    res.locals.siteName = 'Youtube';
-    res.locals.routes = routes;
-    res.locals.user = req.user || {};
-    next();
-}
+	res.locals.siteName = 'Youtube';
+	res.locals.routes = routes;
+	res.locals.user = req.user || null;
+	console.log(req.user);
+	next();
+};
 
 export const uploadVideo = multerVideo.single('videoFile');
--- passport.js
+++ passport.js
@@ -1,8 +1,11 @@
-import passport from "passport";
-import User from "./models/User";
+import passport from 'passport';
+import User from './models/User';
 
+//아이디 찾아서 존재하면 반환
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
 //쿠키에는 오직 user.id만 담아서 보내도록 해
+passport.serializeUser(User.serializeUser());
+
+// user.id로 사용자 찾기
 passport.deserializeUser(User.deserializeUser());
