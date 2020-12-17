e6e50c15a04815673b8f2dc16cded48cf0a1b35c
nibble2 <nibblekk@gmail.com>
2020-12-18 02:28:55 +0900

    Update - userDetail prevent random id

--- controllers/userController.js
+++ controllers/userController.js
@@ -98,10 +98,22 @@ export const getMe = (req, res) => {
 	});
 };
 
-export const userDetail = (req, res) =>
-	res.render('userDetail', {
-		pageTitle: 'User Detail',
-	});
+export const userDetail = async (req, res) => {
+	const {
+		params: {
+			id
+		}
+	} = req;
+	try {
+		const user = await User.findById(id);
+		res.render("userDetail", {
+			pageTitle: "User Detail",
+			user
+		});
+	} catch (error) {
+		res.redirect(routes.home);
+	}
+}
 export const editProfile = (req, res) =>
 	res.render('editProfile', {
 		pageTitle: 'Edit Profile',
