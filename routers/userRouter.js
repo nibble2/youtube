2d952560d0275ec1d3507e1856767c33f6c0a14c
nibble2 <nibblekk@gmail.com>
2020-12-21 03:14:18 +0900

    Update - editProfile, Add change password controller

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,7 +4,8 @@ import {
     userDetail,
     getEditProfile,
     postEditProfile,
-    changePassword
+    getChangePassword,
+    postChangePassword
 } from "../controllers/userController";
 import {
     onlyPrivate,
@@ -16,7 +17,10 @@ const userRouter = express.Router();
 
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
-userRouter.get(routes.changePassword, onlyPrivate, changePassword);
+
+userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
+
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
