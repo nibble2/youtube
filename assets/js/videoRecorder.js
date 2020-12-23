b48124ad944c3e2dcbbb44ca1defc6fc2de2f681
nibble2 <nibblekk@gmail.com>
2020-12-23 23:37:45 +0900

    Update - video recorder stop, save

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -1,18 +1,39 @@
 const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
+
+//밖에서 선언한 후 어디서든 접근 가능하게
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = event => {
     console.log(event);
+    const {
+        data: videoFile
+    } = event;
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(videoFile);
+    link.download = "recorded.webm";
+    document.body.appendChild(link);
+    link.click();
 };
 
+const stopRecording = () => {
+    videoRecorder.stop();
+    recordBtn.removeEventListener("click", stopRecording);
+    recordBtn.addEventListener("click", getVideo);
+    recordBtn.innerHTML = "Start recording";
+};
+
+//찐으로 저장
 const startRecording = () => {
-    const videoRecorder = new MediaRecorder(streamObject);
+    videoRecorder = new MediaRecorder(streamObject);
     videoRecorder.start();
     videoRecorder.addEventListener("dataavailable", handleVideoData);
+    recordBtn.addEventListener("click", stopRecording);
 };
 
+//권한을 허용할때까지 기다려야해서 await사용
 const getVideo = async () => {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({
@@ -36,6 +57,7 @@ const getVideo = async () => {
     }
 };
 
+//버튼 클릭
 function init() {
     recordBtn.addEventListener("click", getVideo);
 }
