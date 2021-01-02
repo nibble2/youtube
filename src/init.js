663f5b55eda1ca9c0c2e18e2f0edfb9ec7b8193b
nibble2 <nibblekk@gmail.com>
2021-01-02 13:19:08 +0900

    Update - change port

--- src/init.js
+++ src/init.js
@@ -9,7 +9,7 @@ import './models/User';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4005;
 
 const handleListening = () => {
 	console.log(`✅  Listening on: http://localhost:${PORT}`);
