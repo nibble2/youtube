8fe2aca4c256cef06dba3b2c5d71610ae377056b
nibble2 <nibblekk@gmail.com>
2021-04-06 19:43:06 +0900

    Update - editVideo

--- src/db.js
+++ src/db.js
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 mongoose.connect(process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL, {
-	useNewUrlParser: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
